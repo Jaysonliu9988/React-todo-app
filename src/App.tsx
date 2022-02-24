fdacb34f79ce2f2f706ca9bd3357bec0e6748790
jayson <leobest9688@gmail.com>
2022-02-24 23:57:01 +0800

    add header and part form

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {TextField, Switch, TableContainer, Paper, Table, TableHead, TableRow, TableBody, TableCell} from '@material-ui/core';
 
 function App() {
   fetch('api/users')
@@ -13,8 +14,36 @@ function App() {
 
 
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className='App'>
+      <div className='top'>
+        <div className='func'>
+          <p>Project Name</p>
+          <TextField id="outlined-basic" variant="outlined" />
+        </div>
+        <div className='func'>
+          <p>User</p>
+          {/* <Select /> */}
+        </div>
+        <div className='func'>
+          <p>Completed</p>
+          <Switch />
+        </div>
+      </div>
+
+      <div className="table-container">
+        <TableContainer component={Paper}>
+          <Table aria-label='simple table'>
+            <TableHead>
+              <TableRow>
+                <TableCell align='center'>Name</TableCell>
+                <TableCell align='center'>User</TableCell>
+                <TableCell align='center'>Completed</TableCell>
+                <TableCell align='center'>Actions</TableCell>
+              </TableRow>
+            </TableHead>
+          </Table>
+        </TableContainer>
+      </div>
     </div>
   );
 }
